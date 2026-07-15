6763f1ea27dad710bf908775870268d3872d5a07
Adrian <123868573+Agonista-receptora-GABA@users.noreply.github.com>
2026-07-15 16:55:01 +0200

    change app.js
    It will be removed finally, but it's for testing purposes.

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 const http = require("http");
 
-http.createServer(function (req, res) {
-
-res.write("On the way to being a full stack engineer!");
-res.end();
-
-}).listen(3000);
+http
+  .createServer(function (req, res) {
+    res.write("API building in progress...");
+    res.end();
+  })
+  .listen(3000);
 
 console.log("Server started on port 3000");
-
